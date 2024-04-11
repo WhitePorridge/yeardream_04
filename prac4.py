f2c439843ff4d6dc10817566007b9ffe2a826a77
JayG <WhitePorridge@users.noreply.github.com>
2024-04-11 16:03:47 +0900

    prac4_Jay-G.py

--- prac4.py
+++ prac4.py
@@ -5,9 +5,10 @@ def read_file():
     file_name을 입력받고, file_name에 해당하는 파일이 있다면, 해당 파일에 내용을 출력하는 코드를 작성하여라.
     단, 만약 파일이 존재하지 않는 경우에는 해당 에러를 예외처리하는 코드를 작성하여라.
     '''
-    file_name = 
+
+    file_name = input()
     try:
-        pass
+        f = open(file_name.txt, "r")
     except :
         pass
 
